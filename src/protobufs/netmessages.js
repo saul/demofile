511fb000cfb679cdd75e4a060d422f10bc66ef94
Saul Rennison <saul.rennison@gmail.com>
2018-10-21 20:57:29 +0100

    Disable TSLint in generated files

--- src/protobufs/netmessages.js
+++ src/protobufs/netmessages.js
@@ -1,4 +1,4 @@
-/*eslint-disable block-scoped-var, id-length, no-control-regex, no-magic-numbers, no-prototype-builtins, no-redeclare, no-shadow, no-var, sort-vars*/
+/* tslint:disable */
 import * as $protobuf from "protobufjs/minimal";
 
 // Common aliases
