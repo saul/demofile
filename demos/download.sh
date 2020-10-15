3395145647df16f51d3c8b43f3639bee74606c18
Saul Rennison <saul.rennison@gmail.com>
2020-10-15 17:03:14 +0100

    Add another end-to-end test

--- demos/download.sh
+++ demos/download.sh
@@ -6,8 +6,9 @@ set -e
 sudo apt install unrar
 
 curl 'http://demos.hltv.org//demo/demofiles/demosection//ESEA-MDL-Season34-Australia-pc419-vs-chiefs-mirage.rar' --output ESEA-MDL-Season34-Australia-pc419-vs-chiefs-mirage.rar
+curl 'http://demos.hltv.org//demo/demofiles/demosection//IEM-New-York-2020-Europe-vitality-vs-faze-bo3.rar' --output IEM-New-York-2020-Europe-vitality-vs-faze-bo3.rar
 
 # Downloaded over insecure http, so validate hashes
 sha256sum -c SHA256SUMS
 
-unrar x -y ESEA-MDL-Season34-Australia-pc419-vs-chiefs-mirage.rar
+unrar x -y "*.rar"
--- tests/end-to-end.test.ts
+++ tests/end-to-end.test.ts
@@ -2,56 +2,101 @@ import * as fs from "fs";
 import { join } from "path";
 import { DemoFile } from "../src/index";
 
+// Some headroom to parse large demo files
+jest.setTimeout(5 * 60 * 1000);
+
 const root = join(__dirname, "..");
 
-it("parses real world GOTV demo", done => {
-  // 5 minute timeout to parse the whole demo
-  jest.setTimeout(5 * 60 * 1000);
-
-  const demo = new DemoFile();
-  const timeline: string[] = [];
-
-  function log(name: string, data: any) {
-    const line = `[${demo.currentTick}] ${name}: ${JSON.stringify(data)}`;
-    timeline[timeline.length] = line;
-  }
-
-  demo.on("start", () => {
-    log("start", demo.header);
-  });
-
+function monitorProgress(name: string, demo: DemoFile) {
   let lastProgressTime = 0;
   demo.on("progress", frac => {
     const now = Date.now();
     if (now - lastProgressTime > 5000) {
       lastProgressTime = now;
-      console.log(`Progress: ${(frac * 100) | 0}%`);
+      console.log(`[${name}] Progress: ${(frac * 100) | 0}%`);
     }
   });
 
-  demo.gameEvents.on("event", e => {
-    if (e.name === "player_footstep") return;
-    log("game_event", e);
+  let startTime = 0;
+  demo.on("start", () => {
+    startTime = Date.now();
   });
 
-  fs.readFile(join(root, "demos/pc419-vs-chiefs-mirage.dem"), (err, buffer) => {
+  demo.on("end", () => {
+    if (startTime === 0) return;
+
+    // Some demos do not have complete header information.
+    // Treat current tick at the end of the demo as the total number of ticks.
+    let totalTicks = demo.header.playbackTicks || demo.currentTick;
+
+    let elapsedSecs = (Date.now() - startTime) / 1000;
+    console.log(
+      `[${name}] Parsed ${totalTicks} ticks in ${elapsedSecs.toFixed(
+        2
+      )} secs (${(totalTicks / elapsedSecs) | 0} ticks/sec)`
+    );
+  });
+}
+
+function startParsing(demoFileName: string, demo: DemoFile) {
+  fs.readFile(join(root, "demos", demoFileName), (err, buffer) => {
     expect(err).toBeFalsy();
-
-    const startTime = Date.now();
     demo.parse(buffer);
-
-    demo.on("end", e => {
-      expect(e.error).toBeFalsy();
-      //expect(timeline).toMatchSnapshot();
-
-      let elapsedSecs = (Date.now() - startTime) / 1000;
-      console.log(
-        `Parsed ${demo.header.playbackTicks} ticks in ${elapsedSecs.toFixed(
-          2
-        )} secs (${(demo.header.playbackTicks / elapsedSecs) | 0} ticks/sec)`
-      );
-
-      done();
-    });
   });
-});
+}
+
+test.concurrent(
+  "parses real world GOTV demo",
+  () =>
+    new Promise(resolve => {
+      const demoFileName = "pc419-vs-chiefs-mirage.dem";
+      const demo = new DemoFile();
+      monitorProgress(demoFileName, demo);
+
+      const timeline: string[] = [];
+
+      function log(name: string, data: any) {
+        const line = `[${demo.currentTick}] ${name}: ${JSON.stringify(data)}`;
+        timeline[timeline.length] = line;
+      }
+
+      demo.on("start", () => {
+        log("start", demo.header);
+      });
+
+      demo.gameEvents.on("event", e => {
+        if (e.name === "player_footstep") return;
+        log("game_event", e);
+      });
+
+      demo.userMessages.on("message", e => {
+        log("user_message", e);
+      });
+
+      demo.on("end", e => {
+        expect(e.error).toBeFalsy();
+        //expect(timeline).toMatchSnapshot();
+        resolve();
+      });
+
+      startParsing(demoFileName, demo);
+    })
+);
+
+test.concurrent(
+  "incomplete demos can be fully parsed",
+  () =>
+    new Promise(resolve => {
+      const demoFileName = "vitality-vs-faze-m1-mirage.dem";
+      const demo = new DemoFile();
+      monitorProgress(demoFileName, demo);
+
+      demo.on("end", e => {
+        expect(e.error).toBeFalsy();
+        expect(e.incomplete).toStrictEqual(true);
+        resolve();
+      });
+
+      startParsing(demoFileName, demo);
+    })
+);
