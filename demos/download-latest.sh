f8448bff0547bf02cfc5e44574a9cdca29e15911
Saul Rennison <saul.rennison@gmail.com>
2020-10-17 18:20:57 +0100

    Checkout using actions/checkout@v2

---
+++ demos/download-latest.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Bail if any command fails
+set -e
+
+sudo apt install unrar
+
+#npx ts-node latest-demo.ts
+
+unrar x -y "latest.rar"
+
+# Grab the first file from the archive
+LATEST_FILE=$(unrar lb latest.rar | head -n1)
+mv $LATEST_FILE latest.dem
+
+#rm latest.rar
