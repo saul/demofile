21877bf5f85e32c3fed0de87b97337d56a226ba5
Saul Rennison <saul@users.noreply.github.com>
2020-10-17 18:55:07 +0100

    Fix auto-generation (#183)

---
+++ demos/latest-demo.ts
@@ -0,0 +1,51 @@
+// Downloads the latest GOTV demo from hltv.org
+// tslint:disable:no-console
+
+import * as fs from "fs";
+import { join } from "path";
+import * as request from "request";
+import { Builder, By } from "selenium-webdriver";
+import { Options } from "selenium-webdriver/chrome";
+
+(async () => {
+  const options = new Options().addArguments(
+    "headless",
+    "disable-gpu",
+    "no-sandbox",
+    "disable-dev-shm-usage"
+  );
+
+  const driver = await new Builder()
+    .forBrowser("chrome")
+    .withCapabilities(options)
+    .build();
+
+  try {
+    console.log("Navigating to hltv.org...");
+    await driver.get("https://www.hltv.org/results?content=demo&gameType=CSGO");
+
+    const matchElem = await driver.findElement(
+      By.css(`.result-con > a[href^="/matches/"]`)
+    );
+    console.log(`Viewing match '${await matchElem.getText()}'...`);
+    await matchElem.click();
+
+    const downloadElem = await driver.findElement(
+      By.css(`a[href^="/download/demo/"]`)
+    );
+    return await downloadElem.getAttribute("href");
+  } finally {
+    await driver.quit();
+  }
+})()
+  .then(url => {
+    console.log(`Downloading '${url}'...`);
+    request(url)
+      .on("error", err => {
+        console.error("Request error", err);
+      })
+      .pipe(fs.createWriteStream(join(__dirname, "latest.rar")));
+  })
+  .catch(err => {
+    console.error(err);
+  });
