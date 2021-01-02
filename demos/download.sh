0e4abb1b2969e16deddebd6f4ce4ab4843e1f712
Saul Rennison <saul.rennison@gmail.com>
2021-01-02 13:27:25 +0000

    Update download.sh

--- demos/download.sh
+++ demos/download.sh
@@ -5,8 +5,8 @@ set -e
 
 sudo apt install unrar
 
-curl 'http://demos.hltv.org//demo/demofiles/demosection//ESEA-MDL-Season34-Australia-pc419-vs-chiefs-mirage.rar' --output ESEA-MDL-Season34-Australia-pc419-vs-chiefs-mirage.rar
-curl 'http://demos.hltv.org//demo/demofiles/demosection//IEM-New-York-2020-Europe-vitality-vs-faze-bo3.rar' --output IEM-New-York-2020-Europe-vitality-vs-faze-bo3.rar
+curl 'https://demos.hltv.org//demo/demofiles/demosection//ESEA-MDL-Season34-Australia-pc419-vs-chiefs-mirage.rar' --output ESEA-MDL-Season34-Australia-pc419-vs-chiefs-mirage.rar
+curl 'https://demos.hltv.org//demo/demofiles/demosection//IEM-New-York-2020-Europe-vitality-vs-faze-bo3.rar' --output IEM-New-York-2020-Europe-vitality-vs-faze-bo3.rar
 
 # Downloaded over insecure http, so validate hashes
 sha256sum -c SHA256SUMS
