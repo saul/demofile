c8ca8b502c9c32215078125cec3f37c4eb2e04d7
Saul Rennison <saul@users.noreply.github.com>
2021-01-02 17:36:34 +0000

    Download test .dem files from Azure storage (#194)

--- demos/download.sh
+++ demos/download.sh
@@ -5,8 +5,11 @@ set -e
 
 sudo apt install unrar
 
-curl 'https://demos.hltv.org//demo/demofiles/demosection//ESEA-MDL-Season34-Australia-pc419-vs-chiefs-mirage.rar' --output ESEA-MDL-Season34-Australia-pc419-vs-chiefs-mirage.rar
-curl 'https://demos.hltv.org//demo/demofiles/demosection//IEM-New-York-2020-Europe-vitality-vs-faze-bo3.rar' --output IEM-New-York-2020-Europe-vitality-vs-faze-bo3.rar
+# Originally from https://demos.hltv.org//demo/demofiles/demosection//ESEA-MDL-Season34-Australia-pc419-vs-chiefs-mirage.rar
+curl 'https://demofiledemos.blob.core.windows.net/demoblobs/ESEA-MDL-Season34-Australia-pc419-vs-chiefs-mirage.rar' --output ESEA-MDL-Season34-Australia-pc419-vs-chiefs-mirage.rar
+
+# Originally from https://demos.hltv.org//demo/demofiles/demosection//IEM-New-York-2020-Europe-vitality-vs-faze-bo3.rar
+curl 'https://demofiledemos.blob.core.windows.net/demoblobs/IEM-New-York-2020-Europe-vitality-vs-faze-bo3.rar' --output IEM-New-York-2020-Europe-vitality-vs-faze-bo3.rar
 
 # Downloaded over insecure http, so validate hashes
 sha256sum -c SHA256SUMS
