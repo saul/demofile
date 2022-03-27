4397d7b710dc8a37ea3368586052f9b49c6138fc
Saul Rennison <saul@users.noreply.github.com>
2022-03-27 13:04:43 +0000

    Fix molotov example

--- examples/molotov.ts
+++ examples/molotov.ts
@@ -2,11 +2,11 @@
 Example output:
 
 ...
-Incendiary thrown by PC419 Chrismagnum exploded at (323.90625,-1462.6875,-173.96875)
-Molotov thrown by CHF| yam- exploded at (-739.15625,-868.03125,-165.96875)
-Incendiary thrown by PC419 FluentSwede exploded at (540.90625,-1692.96875,-261.96875)
-Molotov thrown by CHF| HUGHMUNGUS exploded at (-2381.40625,786.5,-124.46875)
-Molotov thrown by CHF| zephh exploded at (-815.5625,275.90625,-166.4375)
+incendiary thrown by PC419 Chrismagnum exploded at (323.90625,-1462.6875,-173.96875)
+molotov thrown by CHF| yam- exploded at (-739.15625,-868.03125,-165.96875)
+incendiary thrown by PC419 FluentSwede exploded at (540.90625,-1692.96875,-261.96875)
+molotov thrown by CHF| HUGHMUNGUS exploded at (-2381.40625,786.5,-124.46875)
+molotov thrown by CHF| zephh exploded at (-815.5625,275.90625,-166.4375)
 Finished.
 */
 
@@ -18,13 +18,9 @@ function parseDemoFile(path: string) {
   const demoFile = new DemoFile();
 
   demoFile.on("molotovDetonate", e => {
-    const isMolotov = e.type === "molotov";
     const pos = e.projectile.position;
-
     console.log(
-      `${isMolotov ? "Molotov" : "Incendiary"} thrown by ${
-        e.thrower.name
-      } exploded at (${pos.x},${pos.y},${pos.z})`
+      `${e.projectile.grenadeType} thrown by ${e.thrower.name} exploded at (${pos.x},${pos.y},${pos.z})`
     );
   });
 
