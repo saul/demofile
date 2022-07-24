4122ec925e112cf443e5a8f3254dd3db78074d11
Saul Rennison <saul@users.noreply.github.com>
2022-07-24 14:43:13 +0100

    Dismiss cookie popup when downloading latest HLTV demo (#369)
    Also regenerate types off latest CSGO
    
    Co-authored-by: GitHub Actions <github-actions@users.noreply.github.com>

--- demos/latest-demo.ts
+++ demos/latest-demo.ts
@@ -4,7 +4,7 @@
 import * as fs from "fs";
 import { join } from "path";
 import * as request from "request";
-import { Builder, By } from "selenium-webdriver";
+import { Builder, By, until } from "selenium-webdriver";
 import { Options } from "selenium-webdriver/chrome";
 
 (async () => {
@@ -20,9 +20,21 @@ import { Options } from "selenium-webdriver/chrome";
     .withCapabilities(options)
     .build();
 
+  driver.manage().setTimeouts({ implicit: 5000 });
+
   try {
     console.log("Navigating to hltv.org...");
     await driver.get("https://www.hltv.org/results?content=demo&gameType=CSGO");
+
+    const oldPage = driver.findElement(By.css("html"));
+
+    console.log("Dismissing cookie popup...");
+    await driver
+      .findElement(By.id("CybotCookiebotDialogBodyButtonDecline"))
+      .click();
+
+    // Wait for page to reload
+    await driver.wait(until.stalenessOf(oldPage));
 
     const matchElem = await driver.findElement(
       By.css(`.result-con > a[href^="/matches/"]`)
